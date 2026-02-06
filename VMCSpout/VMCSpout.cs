f680bcba55b6f385b353546e526b41f18a09184c
Snow1226 <snow.gbfdiscord@gmail.com>
2026-02-06 23:10:55 +0900

    Version bump

--- VMCSpout/VMCSpout.cs
+++ VMCSpout/VMCSpout.cs
@@ -14,7 +14,7 @@ namespace VMCSpout
 {
     [VMCPlugin(
     Name: "VMC Spout",
-    Version: "0.0.2",
+    Version: "0.1.1",
     Author: "snow",
     Description: "Spout2 sender for VMC",
     AuthorURL: "https://twitter.com/snow_mil",
